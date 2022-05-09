1ddf47079611d2e531bef5a189f597a339e57cf8
河村龍 <kawamuraryuu@kawamuraryuunoMacBook-Air.local>
2022-05-09 20:58:57 +0900

    made ft_lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -2,5 +2,25 @@
 
 t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
+    t_list  *start;
+    t_list  *cur;
 
+    start = ft_lstnew(f(lst->content);
+    if (start == NULL)
+        return (NULL);
+    cur = start;
+    while (1)
+    {
+        if (lst->next == NULL)
+            break ;
+        cur->next = ft_lstnew(f(lst->content));
+        if (cur->next == NULL)
+        {
+            ft_lstclear(&start, del);
+            return (NULL);
+        }
+        cur = cur->next;
+        lst = lst->next;    
+    }
+    return (start);
 }
