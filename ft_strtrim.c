f652355dc78816b96beec9c1dba066187038999a
kawamuraryuu <kawamuraryuu@kawamuryuunoair.(none)>
2022-06-03 23:53:30 +0900

    retry

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -11,7 +11,6 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-
 char	*ft_strtrim(char const	*s1, char const	*set)
 {
 	char	*trim;
@@ -23,7 +22,7 @@ char	*ft_strtrim(char const	*s1, char const	*set)
 		if (*s1 == '\0' || !set)
 			return (ft_strdup(s1));
 		front_finder = 0;
-		while (ft_strchr(set, s1[front_finder]) != NULL)
+		while (s1[front_finder] && ft_strchr(set, s1[front_finder]) != NULL)
 			front_finder++;
 		back_finder = ft_strlen(s1);
 		while (ft_strchr(set, s1[back_finder]) != NULL && \
@@ -35,32 +34,23 @@ char	*ft_strtrim(char const	*s1, char const	*set)
 	return (NULL);
 }
 
-//#include <stdio.h>
+// #include <stdio.h>
 // int main()
 // {
-//     char *result;
-//     result = ft_strtrim("a", NULL);
-//     printf("%s\n", result);
+//     // char *result;
+//     // result = ft_strtrim("a", NULL);
+//     // printf("%s\n", result);
 
 //     char *result2;
 //     char *ss = ",,,,abcd,,,";
 //     result2 = ft_strtrim(ss, ",");
 //     printf("%s\n", result2);
 
-//     char *result3;
-//     result3 = ft_strtrim(NULL, ",");
-//     printf("%s\n", result3);
+//     // char *result3;
+//     // result3 = ft_strtrim(NULL, ",");
+//     // printf("%s\n", result3);
 
-//     char *result4;
-//     result4 = ft_strtrim(NULL, ",");
-//     printf("%s\n", result4);
-
-// printf("--------question------------\n");
-//     char *result5;
-//     result5 = ft_strtrim(",,,,,,", ",");
-//     printf("%s\n", result5);
-
-//     char *result6;
-//     result6 = ft_strtrim(",,,,abcd,,,", ",,,,abcd,,,");
-//     printf("%s\n", result6);
+//     // char *result4;
+//     // result4 = ft_strtrim(NULL, ",");
+//     // printf("%s\n", result4);
 // }
